53f8c5456f76e667138cd195828d666325f6c43e
CoderAryanYadav <Coderaryanyadav@gmail.com>
2025-09-02 01:00:48 +0530

    Initial commit

---
+++ deploy-commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Replace YOUR_USERNAME with your actual GitHub username
+echo "Replace YOUR_USERNAME with your actual GitHub username in the command below:"
+echo ""
+echo "git remote add origin https://github.com/YOUR_USERNAME/opskill.git"
+echo "git push -u origin main"
+echo ""
+echo "After running these commands, go to vercel.com to deploy!"
